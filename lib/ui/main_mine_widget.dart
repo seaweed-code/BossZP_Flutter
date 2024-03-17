d53d44649a153c1582366b35c79a639a6cbe467c
Marvel <marvel@gmail.com>
2024-03-17 16:12:35 +0800

    Update main_mine_widget.dart

--- lib/ui/main_mine_widget.dart
+++ lib/ui/main_mine_widget.dart
@@ -1,11 +1,13 @@
 import 'package:bosszp/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MainMineWidget extends StatelessWidget {
   const MainMineWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final avatarWidth = 50.0;
     return Stack(
       alignment: Alignment.topCenter,
       children: [
@@ -21,7 +23,17 @@ class MainMineWidget extends StatelessWidget {
               surfaceTintColor: Colors.transparent,
             ),
             SliverToBoxAdapter(
-              child: Placeholder(),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: Row(
+                  children: [
+                    ClipRRect(
+                        borderRadius: BorderRadius.circular(avatarWidth / 2),
+                        child: Assets.images.avatar1Iphone
+                            .image(width: avatarWidth, height: avatarWidth))
+                  ],
+                ),
+              ),
             ),
             SliverToBoxAdapter(
               child: Placeholder(),
