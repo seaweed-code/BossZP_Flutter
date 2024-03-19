837ffdc3c3a197fe2505e286a81f9ff71a779b7b
Marvel <marvel@gmail.com>
2024-03-19 15:55:53 +0800

    Update message_list_widget.dart

--- lib/ui/message_list_widget.dart
+++ lib/ui/message_list_widget.dart
@@ -1,19 +1,30 @@
-import 'package:bosszp/model/appearance.dart';
+import 'package:bosszp/gen/assets.gen.dart';
+// import 'package:bosszp/model/appearance.dart';
 import 'package:bosszp/ui/message_item_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+// import 'package:provider/provider.dart';
 
 class MessageListWidget extends StatelessWidget {
   const MessageListWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    context.read<Appearance>();
-    return ListView.builder(
-      itemCount: 100,
-      itemBuilder: (context, index) {
-        return MessageItemWidget();
-      },
+    // context.read<Appearance>();
+
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+                background: Assets.images.basicBgNaviBackImgIphone
+                    .image(fit: BoxFit.cover))),
+        SliverList.builder(
+          itemCount: 30,
+          itemBuilder: (context, index) {
+            return MessageItemWidget();
+          },
+        )
+      ],
     );
   }
 }
