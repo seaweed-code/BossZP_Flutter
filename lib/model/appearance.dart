8b39002133a47fc4cca2741895684ba648f6b599
Marvel <marvel@gmail.com>
2024-03-19 16:41:41 +0800

    Update appearance.dart

--- lib/model/appearance.dart
+++ lib/model/appearance.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Appearance {
   var backgroundColor = const Color(0xFFF5F5F5);
   var titleColor = Colors.black;
-  var subTitleColor = Colors.black87;
+  var subTitleColor = const Color(0xFF5e5e5e);
   var darkColor = const Color(0xFF848484);
   var blueColor = const Color(0xFF00888C);
 }
