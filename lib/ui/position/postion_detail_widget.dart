9de6fd977f5c11fea1b449a9fd67c8ee68d2341d
Marvel <marvel@gmail.com>
2024-03-20 16:59:52 +0800

    Update postion_detail_widget.dart

--- lib/ui/position/postion_detail_widget.dart
+++ lib/ui/position/postion_detail_widget.dart
@@ -5,7 +5,7 @@ import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 
 class PostionDetailWidget extends StatelessWidget {
-  const PostionDetailWidget({super.key, this.leading = 15});
+  const PostionDetailWidget({super.key, this.leading = 25});
   final double leading;
   @override
   Widget build(BuildContext context) {
@@ -15,15 +15,43 @@ class PostionDetailWidget extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-            child: CustomScrollView(
-              slivers: [],
+            child: ListView(
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(leading, 10, leading, 10),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            "iOS技术专家",
+                            style: TextStyle(
+                                color: appear.titleColor,
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Spacer(),
+                          Text(
+                            "40-60K 15薪",
+                            style: TextStyle(
+                                color: appear.blueColor,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
           SafeArea(
             child: Container(
               color: appear.blueColor,
               width: double.infinity,
-              padding: EdgeInsets.only(left: leading, right: leading),
+              height: 60,
+              margin: EdgeInsets.only(left: leading, right: leading),
               child: TextButton(
                 onPressed: () {},
                 child: Text(
